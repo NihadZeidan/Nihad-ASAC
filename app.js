7565d562456bde6dbfc0b8d0c37c644b4be6e8e1
NihadZeidan <nihadzeidan@gmail.com>
2021-02-02 16:59:12 +0200

    adding css

--- app.js
+++ app.js
@@ -1,40 +1,29 @@
-var userName = prompt('Welcome to my page, Please enter your name!');
+// var userName = prompt('Welcome to my page, Please enter your name!');
 
 
-alert('Hello '+ userName);
+// alert('Hello '+ userName);
 
 
-var age = prompt('please enter your age?');
-while(age < 18){
-    age = prompt('Sorry, you must be 18 or above!');
- }
-if(age >= 18) {
-    alert('your age is ' + age + ', '+'then you\'re welcome');
-} 
+// var age = prompt('please enter your age?');
+// while(age < 18){
+//     age = prompt('Sorry, you must be 18 or above!');
+//  }
+// if(age >= 18) {
+//     alert('your age is ' + age + ', '+'then you\'re welcome');
+// } 
 
-var rating =  prompt('Please rate my website!');
-
-
-document.write('<br>'+ 'You gave us ' + rating + ' stars' + '<br/>' );
+// var rating =  prompt('Please rate my website!');
 
 
-// // rating = typeof 55
+// document.write('<br>'+ 'You gave us ' + rating + ' stars' + '<br/>' );
 
-// while( rating != typeof 6) {
-//     rating = prompt('Please enter only numbers!');
+
+// for(i=0; i<rating; i++) {
+//     document.write('<img src="Images/estrelas-desenho-png-4.png" width= 50px />');
 // }
 
-
-for(i=0; i<rating; i++) {
-    document.write('<img src="Images/estrelas-desenho-png-4.png" width= 50px />');
-}
-
-function myFunction(p1, p2) {
-    return p1 + p2;
-  } 
-  console.log(myFunction(2, 4));
-  document.write(myFunction(4, 4))
-
-
-
-
+// function myFunction(p1, p2) {
+//     return p1 + p2;
+//   } 
+//   console.log(myFunction(2, 4));
+//   document.write(myFunction(4, 4))
